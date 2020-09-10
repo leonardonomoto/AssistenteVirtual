4930de084b8042332d551d4ce742f9734267b47d
Leonardo Moadie Nomoto <leonardomnomoto@gmail.com>
2020-09-10 13:12:54 -0300

    Correção desligar.py

--- Desligar.py
+++ Desligar.py
@@ -4,7 +4,7 @@ def comando(cmd):
     os.system(cmd)
 
 def desligar():
-    t = int(raw_input('Quantos minutos para desligar?'))
+    t = int(input('Quantos minutos para desligar?'))
     t = str(t*60)
     cmd = 'shutdown -s -f -t' + (t)
     comando(cmd)
