02af2ac7f094af448ad4b96359f39aaf46c10fc7
Leonardo Moadie Nomoto <leonardomnomoto@gmail.com>
2020-09-17 13:50:53 -0300

    Testando

--- Jarvis.py
+++ Jarvis.py
@@ -3,14 +3,15 @@ from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 import os
 
-bot = ChatBot('Pedro')
-
+bot = ChatBot('Jarvis', read_only=True)
+"""
 bot.set_trainer(ListTrainer) #definir treinamento
 
-for __file__ in os.listdir('chats'):
-    lines = open('chats/' +__file__, 'r').readlines()
-    bot.train(lines)
-"""
+
+for __file__ in os.listdir('chats'): #percorre todos os arquivos em chats
+    lines = open('chats/' +__file__, 'r').readlines() #Lê as linhas 
+    bot.train(lines)"""
+
 r = sr.Recognizer()
 with sr.Microphone() as s:
     r.adjust_for_ambient_noise(s)
@@ -18,4 +19,5 @@ with sr.Microphone() as s:
     while True:
         audio = r.listen(s)
         speech = r.recognize_google(audio, language='pt')
-        print('Você disse: ', speech) """
+        print('Você disse: ', speech) 
+        print('Bot: ', bot.get_response(speech))
---
+++ TesteSR.py
@@ -0,0 +1,1 @@
+print('Fale algo')
